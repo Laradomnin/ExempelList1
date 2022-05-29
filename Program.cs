6ca8d061106d0455b439a4d23ee12ac3784c91a3
Laradomnin <lara.domnin@hotmail.com>
2022-05-29 23:03:00 +0200

    ny

--- Program.cs
+++ Program.cs
@@ -1,18 +1,8 @@
-﻿
-
-
-
-public class Example
+﻿public class Example
 {
     public static void Main()
     {
-
-        string[] names =
-        { "Adam", "Anna", "Lena", "Vasi", "Axel", "Olle" };
-        LinkedList<string> namn = new LinkedList<string>(names);
-
-
-        static void Display(LinkedList<string> nm, string test)
+        void Display(LinkedList<string> nm, string test)
         {
             Console.WriteLine(test);
             foreach (string name in nm)
@@ -22,38 +12,43 @@ public class Example
             Console.WriteLine();
             Console.WriteLine();
 
-            Display(namn, "Finns i List nu:");
 
-            Console.WriteLine("Ange + för att lägga till ett namn eller - för att ta bort ett namn");
-            string v;
-            v = Console.ReadLine();
-            switch (v)
-            {
-                case "+":
-                    Console.WriteLine("Ange ett namn. ");
-                    string? input = Console.ReadLine();
-                    namn.AddFirst(input);
-                    Display(namn, "Namn som finns i listan nu ");
-                    break;
-
-                    case "-":
-                    Console.WriteLine("Välj vilket namn skall tas bort från lista? ");
-                    string? input2 = Console.ReadLine();
-                    namn.Remove(input2);
-                    Display(namn, "har tagits bort namnet  " + (input));
-                    break;
-            }
-
-            {
-               
-              
-            }
+        }
+        // Create the link list.
+        string[] names =
+            { "Adam", "Anna", "Lena", "Vasi", "Axel", "Olle" };
+        LinkedList<string> namn = new LinkedList<string>(names);
+        Display(namn, "Finns i List nu:");
+        bool done = false;
+        char v;
+        do
+        {
+            Console.WriteLine("Lägg till namn ange + ta bort namn - ");
+            v = Convert.ToChar(Console.ReadLine());
             
 
 
+            switch (v)
+            {
+                case '+':
+
+                    Console.WriteLine("Ange ett namn. ");
+                    string? input = Console.ReadLine();
+                    namn.AddFirst($"{input}");
+                    Display(namn, "Namn som finns i listan nu ");
+                    break;
+
+                case '-':
+
+                    Console.WriteLine("Välj vilket namn skall tas bort från lista? ");
+                    string? current = Console.ReadLine();
+                    namn.Remove($"{current}");
+                    Display(namn, "har tagits bort namnet  " + (current));
+                    break;
+
             }
-        }
+        } while (!done);
+    }
+}
 
- }
 
- 
